6cedceaae01e6536664905fd96c077a650597368
ChanderG <chandergovind@gmail.com>
2015-01-06 12:22:36 +0530

    First attempt at more

---
+++ README.md
@@ -0,0 +1,7 @@
+#Unix-system-programs
+
+###What?
+Following the set of excellent notes on Unix system programming by Stewart Weiss.
+[http://compsci.hunter.cuny.edu/~sweiss/course_materials/csci493.66/csci493.66_spr12.php]
+
+
---
+++ more/more.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+#define LINELEN 512
+#define SCREEN_SIZE 23
+#define SPACEBAR 1
+#define RETURN 2
+#define QUIT 3
+#define INVALID 4
+
+//actual function
+void do_more_of(FILE* fp);
+
+//get user input
+int get_user_intput();
+
+int main(int argc, char *argv[]){
+  FILE *fp;
+  int i = 1;
+  if(argc == 1)
+    do_more_of( stdin );
+  else
+    while(i < argc){
+      fp = fopen( argv[i], "r");
+      if( fp != NULL ){
+        do_more_of(fp);
+	fclose(fp);
+      }	
+      else 
+        printf("Skipping %s\n", argv[i]);
+      i++;
+    }
+  return 0;
+}
+
+void do_more_of(FILE* fp){
+  char line[LINELEN];
+  int no_of_lines = SCREEN_SIZE;
+  int getmore = 1;
+  int reply;
+
+  while(getmore && fgets(line, LINELEN, fp)){
+    if(no_of_lines == 0){
+      reply = get_user_intput();
+      switch(reply){
+        case SPACEBAR: no_of_lines = SCREEN_SIZE;
+	               break;
+	case RETURN: no_of_lines++;
+	             break;
+	case QUIT: getmore = 0;
+	           break;
+	default: break;
+      }
+    }
+    if(fputs(line, stdout) == EOF) exit(1);
+    no_of_lines--;
+  }
+  return;
+}
+
+int get_user_intput(){
+  int c;
+  printf("\033[7m more? \033[m");
+  while((c = getchar()) != EOF) 
+    switch(c){
+      case ' ' : return SPACEBAR;
+      case 'q' : return QUIT;
+      case '\n' : return RETURN;
+      defualt: return INVALID;
+    }
+}
