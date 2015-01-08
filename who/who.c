5dbf3f5f0dc47bfd280b75a27940665e46a48330
ChanderG <chandergovind@gmail.com>
2015-01-08 12:12:31 +0530

    funtioning who

--- who/who.c
+++ who/who.c
@@ -6,7 +6,8 @@
 #include<fcntl.h> //for open
 
 void main(){
-   
+  struct utmp utmp_record; 
+
   //open the utmp file for reading
   int fd;  //file descriptor
   fd = open(UTMP_FILE, O_RDONLY);
@@ -16,6 +17,24 @@ void main(){
   }
 
   //until end of file keep reading from the file
+  int status;
+  int size_utmp_record = sizeof(struct utmp);
+  while(1){
+    status = read(fd, (void*)(&utmp_record), size_utmp_record);
+    if(status == -1){
+      perror("Error in reading UTMP_FILE: ");
+      exit(1);
+    }
+    else if(status == 0){
+      break;
+    }  
+    else if(status == size_utmp_record){
+      //actual work here
+      printf("%s\t%s\n", utmp_record.ut_user, utmp_record.ut_line);
+    }
+  }
+
+  close(fd);
 
   return;
 }
