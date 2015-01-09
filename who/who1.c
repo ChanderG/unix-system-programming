5c638968836a07846ac2ed1d204c64e9cea21c81
ChanderG <chandergovind@gmail.com>
2015-01-09 19:42:51 +0530

    Add version 2 of who program

---
+++ who/who1.c
@@ -0,0 +1,29 @@
+//who program ver 2
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<utmp.h>  //for utmp related work
+#include<time.h>
+
+void main(){
+  struct utmp* utmp_record; 
+
+  setutent();
+
+  while(1){
+    utmp_record = getutent();
+
+    if(utmp_record == NULL){
+      break;
+    }
+    else 
+      //actual work here
+      if(utmp_record->ut_type == USER_PROCESS){
+	printf("%s %12s", utmp_record->ut_user, utmp_record->ut_line);
+	printf("\t%12.12s\n", ctime(&utmp_record->ut_time) + 4);
+      }
+    }
+
+  endutent();
+  return;
+}
